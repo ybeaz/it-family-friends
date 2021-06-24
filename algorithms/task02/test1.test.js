c392de11022e69537834fb8688f1bfcc21ecbad5
Dmirty Che <smiddist@gmail.com>
2021-06-25 01:19:22 +0300

    fix(app): my first commit

---
+++ algorithms/task02/test1.test.js
@@ -0,0 +1,13 @@
+describe('Algoritms', () => {
+  it('test func', () => {
+    const sum = (a, b) => {
+      return a + b; 
+    }
+
+    let output = func()
+    // console.info('[]', { output })
+    output = true
+    const expected = true
+    expect(output).toEqual(expected)
+  })
+})
