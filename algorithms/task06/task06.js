f03bd1634aab8dfc373111363133b5cab0c18a92
Smiddist <57263895+Smiddist@users.noreply.github.com>
2021-07-09 17:57:10 +0300

    Update task06.js

--- algorithms/task06/task06.js
+++ algorithms/task06/task06.js
@@ -1,16 +1,11 @@
-let a = 0;
+let a = 5.5;
 function IsInteger(a) {
-  if (isNaN(a)){
-    return false;
+  if (a%1 === 0){
+    return true;
   }
   else {
-    return true;
+    return false;
   }
 }
 console.info(IsInteger(a));
-<<<<<<< HEAD
-
 console.info(Number.isInteger(a));
-=======
-console.info(Number.isInteger(a));
->>>>>>> 8186cd47472a4f9eb27c2ed40a69bb8bb0410286
