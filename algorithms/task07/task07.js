a0d7405172c6cfe683128f169d2d132ff1e7f5aa
Dmirty Che <smiddist@gmail.com>
2021-07-11 21:25:21 +0300

    fix(app): add task07 ChD

--- algorithms/task07/task07.js
+++ algorithms/task07/task07.js
@@ -1,17 +1,12 @@
-let a = 97;
-function isPrime(a) {
+let a = 13;
+const isPrime = (a)  => {
   let j = false;
   let i = 2;
-  for (i = 2; i*i <= a && j != true; i++){
+  for (i = 2; i < a && j !== true; i++){
     if (a % i === 0){
       j = true;
     }
   }
-    if (j === true) {
-      return false;
-    }
-    else {
-      return true;
-    }
+      return j !== true;
 }
 console.info(isPrime(a));
