c2e229d7af2da11ca79059e56ff10cd68f38e746
Dmirty Che <smiddist@gmail.com>
2021-07-11 21:01:52 +0300

    fix(app): add task08 ChD

--- algorithms/task08/task08.js
+++ algorithms/task08/task08.js
@@ -2,8 +2,13 @@ const arr = [1, 2, 1, 'min', 'max', 'max', 'min']
 
 
 function removeDuplicateMembersArray(arr) {
-  const set = new Set(arr);
-  return set;
+  let arr1 = [];
+
+  for (let value of arr)
+    if (!arr1.includes(value)) {
+      arr1.push(value)
+    }
+  return arr1;
 }
 let a = removeDuplicateMembersArray(arr); // [1, 2, 'min', 'max']
-console.info(a);
+console.log(a);
