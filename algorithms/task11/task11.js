e3ff2f12b5692f11b08f933eb782bcdd33d8411b
Dmirty Che <smiddist@gmail.com>
2021-08-19 21:59:16 +0300

    fix(app): add task13 ChD

--- algorithms/task11/task11.js
+++ algorithms/task11/task11.js
@@ -2,16 +2,17 @@
 Description: to suggest (an object) that helps write letters with predefined congratulation as a private variable
 File name: letterTemplate.test.js
 */
+
 const letterTemplate = {
-  op: "Hey",
-  fin: "Yours, Anna",
-  write: function (str, str1) {
-  const sign = ' Yours, Anna';
-    const res = 'Hey, ' + str + ' ' + str1 + sign;
-    console.info(res);
-  return res;
-    }
-};
+  op: 'Hey',
+  fin: 'Yours, Anna',
+  write(str, str1) {
+    const sign = ' Yours, Anna'
+    const res = 'Hey, ' + str + ' ' + str1 + sign
+    console.info(res)
+    return res
+  },
+}
 
 const a = letterTemplate.write('Marina', 'Happy Birthday to you!') // ‘Hey, Marina, Happy Birthday to you, Yours, Anna’
 const b = letterTemplate.write('Alex', 'Happy New Year!') // ‘Hey, Alex’, Happy New Year! Yours, Anna’
