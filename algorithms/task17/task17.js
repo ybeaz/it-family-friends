70fc9f2c6241fb98a6394db4324a3bca4facee97
Dmirty Che <smiddist@gmail.com>
2021-09-15 23:10:10 +0300

    html layout

---
+++ algorithms/task17/task17.js
@@ -0,0 +1,7 @@
+/*
+Description: to get sub sums of the array from the beginning to the next item
+File name: getSubSumsArr.test.js
+*/
+
+const arrIn = [1, 2, 3, 4, 5]
+getSubSumsArr(arr) // [ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5 ] = [ 1, 3, 6, 10, 15 ]
