656e1832b7275a0df1f7a174a629ec65aa8e80a0
Dmirty Che <smiddist@gmail.com>
2021-10-05 11:43:40 +0300

    fix(app): add task17 h

--- algorithms/task17/task17.js
+++ algorithms/task17/task17.js
@@ -1,7 +1,19 @@
 /*
-Description: to get sub sums of the array from the beginning to the next item
+Description: to get sub sums of the array from the beginning to the next item 
 File name: getSubSumsArr.test.js
 */
 
 const arrIn = [1, 2, 3, 4, 5]
-getSubSumsArr(arr) // [ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5 ] = [ 1, 3, 6, 10, 15 ]
+
+const getSubSumsArr = arrIn => {
+  let x = 0
+  let arr = []
+  arrIn.forEach(element => {
+    x = x + arrIn[element]
+    arr.push(x)
+  })
+
+  return arr
+}
+
+console.info(getSubSumsArr(arrIn)) // [ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5 ] = [ 1, 3, 6, 10, 15 ]
